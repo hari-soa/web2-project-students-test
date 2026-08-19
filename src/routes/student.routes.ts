ac710fb6b61b1c443948554def6e760d623562f2
hari-soa <hei.zarasoa@gmail.com>
2026-08-19 08:05:41 +0300

    refactor(routes): rename root.ts to student.routes.ts, update controller function names and import path

---
+++ src/routes/student.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+
+import {
+  getAllStudents,
+  getStudentById,
+  addStudent,
+  updateStudent,
+  updateStudentPartially,
+  deleteStudent,
+} from "../controllers/student.controller";
+
+const router = Router();
+
+router.get("/students", getAllStudents);
+
+router.get("/students/:id", getStudentById);
+
+router.post("/students", addStudent);
+
+router.put("/students/:id", updateStudent);
+
+router.patch("/students/:id", updateStudentPartially);
+
+router.delete("/students/:id", deleteStudent);
+
+export default router;
