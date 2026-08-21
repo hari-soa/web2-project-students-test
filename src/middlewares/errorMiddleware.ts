6e0abfecd6a707d321170f61413c440d248fecad
hari-soa <hei.zarasoa@gmail.com>
2026-08-21 21:09:06 +0300

    feat(middleware): create JWT errors middleware

---
+++ src/middlewares/errorMiddleware.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+
+export const errorMiddleware = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const status = err.status || 500;
+  res
+    .status(status)
+    .json({ success: false, message: err.message || "Internal server error" });
+};
