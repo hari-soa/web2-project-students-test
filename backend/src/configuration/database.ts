a90b13ed0dbf92effcb449fcf087e0d3d4c187a2
hari-soa <hei.zarasoa@gmail.com>
2026-08-22 08:47:06 +0300

    feat: finalize database

--- backend/src/configuration/database.ts
+++ backend/src/configuration/database.ts
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT) || 5432,
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "postgres",
-  database: process.env.DB_NAME || "school_db",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
